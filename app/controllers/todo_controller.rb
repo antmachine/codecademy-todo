da307f847959d0b9ccba916c37cc147feb648e4f
antmachine <anthonysallows@gmail.com>
2014-08-23 21:21:14 -0700

    stubbed out the todos_controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,2 +1,26 @@
 class TodoController < ApplicationController
+
+	def index
+	 	
+	end
+
+	def new
+		
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+
+	end
+
+	def destroy
+		
+	end
 end
